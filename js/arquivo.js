e00e4a1cc525e309d649382fd4b7c442030e5cdf
rafaela <rafaela.fantini01@gmail.com>
2025-07-01 12:26:59 -0300

    Aula de JavaScript 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,29 @@
+var global = "variavel global";
+
+console.log(global);
+
+function exemplo2(){
+    var local = "Variavel local";
+    console.log(local);
+
+}
+
+exemplo2();
+// console.log(local);
+
+let x = 10;
+if (x  > 5)   {
+    let y = 20;
+    console.log("Valor de y: ", y);
+}
+// console.log(y);
+console.log(x);
+
+const_constante = "texto";
+_constante = "outro texto";
+console.log(_constante);
+
+function saudacao(){
+
+    alert("Bem vindo ao nosso site 3!");
+}
